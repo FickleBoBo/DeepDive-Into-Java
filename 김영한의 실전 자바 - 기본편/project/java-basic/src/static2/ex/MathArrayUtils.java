a2420fe9c830a5936e6787b6f4c27965bb4af107
FickleBoBo <yukmw0704@gmail.com>
2025-07-15 16:17:02 +0900

    feat: 김영한의 실전 자바 - 기본편 섹션 8 수강

---
+++ 김영한의 실전 자바 - 기본편/project/java-basic/src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,39 @@
+package static2.ex;
+
+public class MathArrayUtils {
+
+    private MathArrayUtils() {
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static double average(int[] array) {
+        return (double) sum(array) / array.length;
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
