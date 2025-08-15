ae0545ea088a7be01b3fc155099c9494e18b6385
FickleBoBo <yukmw0704@gmail.com>
2025-08-15 19:50:08 +0900

    feat: 김영한의 실전 자바 - 고급 2편, I/O, 네트워크, 리플렉션 섹션 15 수강

--- 김영한의 실전 자바 - 고급 2편, I／O, 네트워크, 리플렉션/project/java-adv2/src/was/httpserver/HttpRequest.java
+++ 김영한의 실전 자바 - 고급 2편, I／O, 네트워크, 리플렉션/project/java-adv2/src/was/httpserver/HttpRequest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.*;
+import static util.MyLogger.log;
 
 public class HttpRequest {
 
@@ -18,7 +19,7 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parseHeaders(reader);
-        // 메시지 바디는 이후에 처리
+        parseBody(reader);  // 추가
     }
 
     private void parseRequestLine(BufferedReader reader) throws IOException {
@@ -51,9 +52,29 @@ public class HttpRequest {
 
     private void parseHeaders(BufferedReader reader) throws IOException {
         String line;
-        while ((line = reader.readLine()).isEmpty()) {
+        while (!(line = reader.readLine()).isEmpty()) {
             String[] headerParts = line.split(":");
             headers.put(headerParts[0].trim(), headerParts[1].trim());
+        }
+    }
+
+    private void parseBody(BufferedReader reader) throws IOException {
+        if (!headers.containsKey("Content-Length")) {
+            return;
+        }
+
+        int contentLength = Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars = new char[contentLength];
+        int read = reader.read(bodyChars);
+        if (read != contentLength) {
+            throw new IOException("Fail to read entire body. Expected " + contentLength + " bytes, but read " + read);
+        }
+        String body = new String(bodyChars);
+        log("HTTP Message Body: " + body);
+
+        String contentType = headers.get("Content-Type");
+        if (contentType.equals("application/x-www-form-urlencoded")) {
+            parseQueryParameters(body);
         }
     }
 
